699ceb3a7d5b5184c27a30fc111aca2e073246af
Rishabh Amar <111600820+rishabhamar@users.noreply.github.com>
2025-08-11 08:42:44 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -82,4 +82,5 @@ def download():
     return send_file("artifacts/final_data.xlsx", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
