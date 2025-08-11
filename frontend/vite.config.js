5a17cd89c9185ec610ffc69e5ccba0d4ecdbe535
Rishabh Amar <111600820+rishabhamar@users.noreply.github.com>
2025-08-11 09:00:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,          // binds to 0.0.0.0
+    strictPort: true,    // uses the port without fallback
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  },
+});
